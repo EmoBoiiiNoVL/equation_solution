fcbc728b2b52541805b5cc0389ab6c70d7cb3988
VladNoz <nozdren.21@mail.ru>
2024-04-18 20:55:07 +0700

    up

--- version.php
+++ version.php
@@ -6,7 +6,7 @@ defined('MOODLE_INTERNAL') || die();
  * @var $plugin stdClass
  */
 $plugin->component = 'block_equation_solution';      // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2022112802;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022112803;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112809;        // Requires this Moodle version.
 
 
